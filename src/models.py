70e92fdd7a8494a3424960d66393b825d4826487
Olman <Olmancampos07@hotmail.com>
2021-04-06 22:34:05 +0000

    instagram DB

--- src/models.py
+++ src/models.py
@@ -8,24 +8,41 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Usuario(Base):
+    __tablename__ = 'usuario'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    nombre = Column(String(250), nullable=False)
+    apellido = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Seguidores(Base):
+    __tablename__ = 'seguidores'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    id_seguidor = Column(String(250), nullable=False)
+    usuarios_id = Column(Integer, ForeignKey('usuario.id'))
 
+class Likes(Base):
+    __tablename__ = 'likes'
+    id = Column(Integer, primary_key=True)
+    id_seguidor = Column(String(250), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    id_seguidor = Column(String(250), nullable=False)
+    descripcion = Column(String(250), nullable=False)
+    photo = Column(String(250), nullable=False)
+    
+
+class Comentarios(Base):
+    __tablename__ = 'comentarios'
+    id = Column(Integer, primary_key=True)
+    comentario = Column(String(250))
+    usuario_id = Column(Integer, ForeignKey('usuario.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    
     def to_dict(self):
         return {}
 
